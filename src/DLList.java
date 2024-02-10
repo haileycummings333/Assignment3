e89f2804dc3297f84750b6609d543b3f6666b469
haileycummings333 <haileycummings333@gmail.com>
2024-02-10 16:58:12 -0600

    question 4,  DLList class outline 2/10

---
+++ src/DLList.java
@@ -0,0 +1,25 @@
+public class DLList {
+    private DLNode head;
+
+    // constructor
+    public DLList() {
+        this.head = null;
+    }
+
+    // add a book to the end of dllist
+    public void listAdd(Book book) {
+
+    }
+
+    // remove a book from the dllist at pos
+    public void listRemove(int pos) {
+
+    }
+
+    // convert the dllist to string
+    @Override
+    public String toString() {
+        return "";
+    }
+
+}
